dbd51466f8200532738fe16db71735db239d96f5
autrijus <autrijus@13bdeeb7-1ef5-0310-ba8f-c9f358a9e96d>
2005-04-19 19:01:59 +0000

    r4760@not (orig r439):  svm | 2005-02-27 12:54:37 +0800  SVM: skipping changes 0-439 for http://svn.elixus.org/depot

---
+++ myldr/mktmpdir.h
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#ifndef X_OK
+#define X_OK 0x04
+#endif
+
+#ifndef S_ISREG
+#define S_ISREG(x) 1
+#endif
+
+#ifndef MAXPATHLEN
+#define MAXPATHLEN 32767
+#endif
+
+#ifdef HAS_LSTAT
+#define par_lstat lstat
+#else
+#define par_lstat stat
+#endif
+
+#if defined(WIN32) || defined(OS2)
+static const char *dir_sep = "\\";
+static const char *path_sep = ";";
+#else
+static const char *dir_sep = "/";
+static const char *path_sep = ":";
+#endif
+
+#ifndef PL_statbuf
+static struct stat PL_statbuf;
+#endif
+
+#include "utils.c"
+#include "sha1.c"
---
+++ myldr/static.c
@@ -0,0 +1,117 @@
+#include "EXTERN.h"
+#include "perl.h"
+
+#undef PL_statbuf
+#undef readdir
+#ifdef WIN32
+#define mkdir(file, mode) _mkdir(file)
+#endif
+
+#include "mktmpdir.c"
+#include "my_perl.c"
+#include "my_par.c"
+
+/*
+extern char * name_load_me_0;
+extern char * name_load_me_1;
+extern unsigned long size_load_me_0;
+extern unsigned long size_load_me_1;
+extern char load_me_0[];
+extern char load_me_1[];
+*/
+
+char *my_file;
+
+int my_mkfile (char* argv0, char* stmpdir, const char* name, unsigned long size) {
+    int i;
+#ifndef PL_statbuf
+    struct stat PL_statbuf;
+#endif
+
+    my_file = (char *)malloc(strlen(stmpdir) + strlen(name) + 5);
+    sprintf(my_file, "%s/%s", stmpdir, name);
+
+    if ( par_lstat(my_file, &PL_statbuf) == 0 ) {
+        if ( (unsigned long)PL_statbuf.st_size == size ) return -2;
+    }
+
+    i = open(my_file, O_CREAT | O_WRONLY | OPEN_O_BINARY);
+
+    if (i == -1) {
+        fprintf(stderr, "%s: creation of %s failed - aborting with %i.\n", argv0, my_file, errno);
+        return 0;
+    }
+
+    return i;
+}
+
+int main ( int argc, char **argv, char **env )
+{
+    int i;
+    char *stmpdir;
+    char *buf = (char *)malloc(MAXPATHLEN);
+#ifdef WIN32
+typedef BOOL (WINAPI *pALLOW)(DWORD);
+    HINSTANCE hinstLib;
+    pALLOW ProcAdd;
+#ifndef ASFW_ANY
+#define ASFW_ANY -1
+#endif
+#endif
+
+    par_init_env();
+    par_mktmpdir( argv );
+
+    stmpdir = (char *)par_getenv("PAR_TEMP");
+    if ( stmpdir != NULL ) {
+        i = mkdir(stmpdir, 0755);
+        if ( (i != 0) && (i != EEXIST) && (i != -1) ) {
+            fprintf(stderr, "%s: creation of private temporary subdirectory %s failed - aborting with %i.\n", argv[0], stmpdir, errno);
+            return 2;
+        }
+    }
+
+    i = my_mkfile( argv[0], stmpdir, name_load_me_0, size_load_me_0 );
+    if ( !i ) return 2;
+    if ( i != -2 ) {
+        WRITE_load_me_0(i);
+        close(i); chmod(my_file, 0755);
+    }
+
+    my_file = par_basename(par_findprog(argv[0], strdup(par_getenv("PATH"))));
+
+    i = my_mkfile( argv[0], stmpdir, my_file, size_load_me_1 );
+    if ( !i ) return 2;
+    if ( i != -2 ) {
+        WRITE_load_me_1(i);
+        close(i); chmod(my_file, 0755);
+    }
+
+    sprintf(buf, "%i", argc);
+    par_setenv("PAR_ARGC", buf);
+    for (i = 0; i < argc; i++) {
+        buf = (char *)malloc(strlen(argv[i]) + 14);
+        sprintf(buf, "PAR_ARGV_%i", i);
+        par_setenv(buf, argv[i]);
+    }
+
+#ifdef WIN32
+    hinstLib = LoadLibrary("user32");
+    if (hinstLib != NULL) {
+        ProcAdd = (pALLOW) GetProcAddress(hinstLib, "AllowSetForegroundWindow");
+        if (ProcAdd != NULL)
+        {
+            (ProcAdd)(ASFW_ANY);
+        }
+    }
+
+    par_setenv("PAR_SPAWNED", "1");
+    i = spawnvpe(P_WAIT, my_file, (const char* const*)argv, (const char* const*)environ);
+#else
+    execvp(my_file, argv);
+    return 2;
+#endif
+
+    par_cleanup(stmpdir);
+    return i;
+}
