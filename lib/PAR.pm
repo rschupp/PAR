82e7f9f20fbede6424885b5b80f7d73f10101a3d
tsee <tsee@13bdeeb7-1ef5-0310-ba8f-c9f358a9e96d>
2006-12-03 17:23:53 +0000

    This is PAR 0.970. Also: Removing inc/Module/Install/PAR.pm.

--- lib/PAR.pm
+++ lib/PAR.pm
@@ -1,5 +1,5 @@
 package PAR;
-$PAR::VERSION = '0.97';
+$PAR::VERSION = '0.970';
 
 use 5.006;
 use strict;
@@ -13,7 +13,7 @@ PAR - Perl Archive Toolkit
 
 =head1 VERSION
 
-This document describes version 0.97 of PAR, released November 29, 2006.
+This document describes version 0.970 of PAR, released December  3, 2006.
 
 =head1 SYNOPSIS
 
