b99612566c9041e2578cdd08223397f6a1306271
tsee <tsee@13bdeeb7-1ef5-0310-ba8f-c9f358a9e96d>
2009-02-20 09:47:23 +0000

    slightly better temp cleanup

--- lib/PAR.pm
+++ lib/PAR.pm
@@ -1074,7 +1074,18 @@ sub _cached_member_named {
 }
 
 
-
+# Attempt to clean up the temporary directory if
+# --> We're running in clean mode
+# --> It's defined
+# --> It's an existing directory
+# --> It's empty
+END {
+  if (exists $ENV{PAR_CLEAN} and $ENV{PAR_CLEAN}
+      and exists $ENV{PAR_TEMP} and defined $ENV{PAR_TEMP} and -d $ENV{PAR_TEMP}
+  ) {
+    rmdir($ENV{PAR_TEMP});
+  }
+}
 
 1;
 
